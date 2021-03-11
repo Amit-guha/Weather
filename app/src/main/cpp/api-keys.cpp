e816a8b98f2456a4a8b0710ee4764773ff5f5a07
Amit guha <amitguho95@gmail.com>
2021-03-11 22:01:13 +0600

    Api

--- app/src/main/cpp/api-keys.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_mindorks_myapplication_APIKeyLibrary_getAPIKey(JNIEnv* env, jobject /* this */) {
-    std::string api_key = "69c6aa61a0f3e87516a4192d1666ebdd";
-    return env->NewStringUTF(api_key.c_str());
-}
